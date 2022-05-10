11be5d459c6b5a5b04a72013c1dcaa487bc6975f
PimTournaye <pimtournaye@student.ehb.be>
2022-05-10 11:26:27 +0200

    init Obstacle class

---
+++ pong/src/Obstacle.js
@@ -0,0 +1,12 @@
+class Obstacle {
+    constructor(x, y, width, height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        this.lifetime;
+        this.text;
+        this.event;
+    }
+}
