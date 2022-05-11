2227c2bf694c11737341389daf0d141bf6eda66a
PimTournaye <pimtournaye@student.ehb.be>
2022-05-11 14:39:37 +0200

    new class init

---
+++ pong/src/Music.js
@@ -0,0 +1,11 @@
+class Music {
+    constructor(){
+        this.currentKey;
+        this.scale;
+        this.currentAvoidNotes;
+        this.tensions;
+        
+        this.nextKey
+
+    }
+}
