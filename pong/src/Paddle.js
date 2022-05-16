236fb12117d19b71fc9cdaaa10aac8e0e27ff90c
PimTournaye <pimtournaye@student.ehb.be>
2022-05-16 12:21:35 +0200

    minor adjustments

--- pong/src/Paddle.js
+++ pong/src/Paddle.js
@@ -4,17 +4,17 @@ export default class Paddle {
         this.y = lowestActive;
         this.width = 10;
         this.height = highestActive - lowestActive;
+        this.active = false;
     }
 
     draw() {
-        fill(200);
+        fill(150, 0, 20);
         rect(this.x, this.y, this.width, this.height);
+        this.active = true;
     }
 
-    update(y, height){
+    updateRect(y, height) {
         this.y = y;
         this.height = height;
     }
 }
-
-    
