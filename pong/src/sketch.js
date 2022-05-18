acd01b8e4578eaaaae92a1a06bb71dbdef4fedce
PimTournaye <pimtournaye@student.ehb.be>
2022-05-18 14:05:30 +0200

    some code cleanup

--- pong/src/sketch.js
+++ pong/src/sketch.js
@@ -1,18 +1,13 @@
 import { WebMidi } from 'webmidi';
-import { p5, sketch } from 'p5js-wrapper';
+import { sketch } from 'p5js-wrapper';
 import Keyboard from './Keyboard';
 import PongBall from './PongBall';
 
+// Different MIDI devices, should probably get a list of devices and put them into a select menu
 let name1 = "Launchkey MK3 49 LKMK3 MIDI Out"
 let name2 = "Keystation 61 MK3"
 let name3 = "KOMPLETE KONTROL M32"
 let name4 = 'IAC Driver Bus 1'
-
-// Set arbirary note number to compare later
-let lowestNoteRight = 54;
-let lowestNoteLeft = 54;
-let highestNoteRight = 54;
-let highestNoteLeft = 55;
 
 let ballX, ballY, div;
 
@@ -33,6 +28,7 @@ WebMidi
     WebMidi.inputs.forEach((device, index) => {
       console.log(`${index}: ${device.name}`);
     });
+    // doing some weird stuff to but it makes WebMidi work properly
     let input1 = WebMidi.getInputByName(name1);
     let input2 = WebMidi.getInputByName(name1);
     inputs.push(input1);
@@ -66,23 +62,18 @@ sketch.setup = () => {
   let direction = createVector(random(-25, 25), random(-25, 25));
   ball = new PongBall(width / 2, height / 2, 20, 2, direction);
 
-
-
 }
 
 // only start draw once the MIDI device is connected and the lowest and highest note are defined and remain the same for at least 10 seconds
 sketch.draw = () => {
   background(220);
-  //getKeyboardRange();
-  // if keyboard range remains the same for at least 10 seconds, stop calling getKeyboardRange()
+  
   ball.update();
 
-  //drawPianoKeys('right');
-  //drawPianoKeys('left');
+  
   PLAYERS.forEach(keyboard => {
     keyboard.update();
   });
 
   ball.checkCollision();
-
 }
