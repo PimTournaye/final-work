08807882b2490746b8753f0f20886df2f14b5365
PimTournaye <pim.tournaye@student.ehb.be>
2022-06-01 18:34:18 +0200

    Platform IO - Reset

--- ESP32 - Sockets/src/main.cpp
+++ ESP32 - Sockets/src/main.cpp
@@ -7,31 +7,43 @@
 
 #include <Adafruit_NeoPixel.h>
 
-#include <WebSocketsClient_Generic.h>
-#include <SocketIOclient_Generic.h>
+// #include <WebSocketsClient_Generic.h>
+// #include <SocketIOclient_Generic.h>
 
-WiFiMulti WiFiMulti;
+#include <WebSocketsClient.h>
+#include <SocketIOclient.h>
+
+// WiFiMulti WiFiMulti;
 SocketIOclient socketIO;
 
 // Network configuration
-const char *ssid = "REPLACE_WITH_YOUR_SSID";
-const char *password = "REPLACE_WITH_YOUR_PASSWORD";
-const char *host = "INSERT HERE HOST ADDRESS";
+const char *ssid = "Medialab";
+const char *password = "Ml@bAdmin!";
+String socketIP = "192.168.2.232";
+int socketPORT = 8880;
 
-// Control panel pins
-int masterSliderPin = 26;  // A0
-int piano1Pin = 25;        // A1
-int piano2Pin = 34;        // A2
-int othersPin = 39;        // A3
-int speedPin = 36;         // A4
+// Control panel pins - Avoiding ADC2 pins since those are used for WiFi -- https://learn.adafruit.com/adafruit-huzzah32-esp32-feather/esp32-faq
+int masterSliderPin = 32;  // A7 or GPIO32
+int piano1Pin = 33;        // A9 or GPIO33
+int piano2Pin = 34;        // A2 or GPIO34
+int othersPin = 39;        // A3 or GPIO39
+int speedPin = 36;         // A4 or GPIO36
+
 int pixelRing = 15;        // D15
-int rotaryPinA = 32;       // D32
-int rotaryPinB = 14;       // D14
 
+int rotaryPinA = 12;       // D12 - CLK
+int rotaryPinB = 14;       // D14 - DT
+int rotaryClick = 13;      // D13 - SW
+
+// Variables for the rotary encoder
 int rotaryCounter = 0;
 int rotaryAState;
 int rotaryALastState;
 
+// NeoPixel setup
+Adafruit_NeoPixel pixels(12, pixelRing, NEO_GRB + NEO_KHZ800);
+
+// map one potentiometer to a range of values
 int getSpeed() {
   float analogValue = analogRead(speedPin);
   return map(analogValue, 0, 1023, 0, 100);
@@ -50,23 +62,7 @@ void rotaryRead() {
   rotaryALastState = rotaryAState;
 }
 
-// String makeJson() {
-
-//   String output;
-
-//   doc["masterVolume"] = analogRead(masterSliderPin);
-//   doc["piano1"] = analogRead(piano1Pin);
-//   doc["piano2"] = analogRead(piano2Pin);
-//   doc["other"] = analogRead(othersPin);
-//   doc["speed"] = getSpeed();
-//   doc["circle"] = rotaryCounter % 12;
-
-//   serializeJson(doc, output);
-
-//   return output;
-// }
-
-
+// Copy pasted from WebSocket / Socket.io example, 
 void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) {
 
   switch (type) {
@@ -132,6 +128,9 @@ void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) 
   }
 }
 
+
+int led = LED_BUILTIN;
+
 ////////////////////
 // MAIN FUNCTIONS //
 ////////////////////
@@ -142,21 +141,23 @@ void setup() {
   Serial.println("");
   Serial.println("");
 
-  for (uint8_t t = 4; t > 0; t--) {
-    Serial.printf("[SETUP] BOOT WAIT %d...\n", t);
-    Serial.flush();
-    delay(1000);
-  }
-
-  // Connect to Wi-Fi
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(500);
   }
 
-  // Log the ESP32 local IP Address
+  // Client address
+  Serial.print("WebSockets Client started @ IP address: ");
   Serial.println(WiFi.localIP());
+
+  // server address, port and URL
+  Serial.print("Connecting to WebSockets Server @ IP address: ");
+  Serial.print(socketIP);
+  Serial.print(", port: ");
+  Serial.println(socketPORT);
 
   // Set up pins
   pinMode(masterSliderPin, INPUT);
@@ -170,10 +171,15 @@ void setup() {
   pinMode(rotaryPinA, INPUT);
   pinMode(rotaryPinB, INPUT);
 
+  // for testing
+  pinMode(led, OUTPUT);
+
   rotaryALastState = digitalRead(rotaryPinA);
 
+  pixels.begin();
+
   // server address, port and URL
-  socketIO.begin("10.11.100.100", 8880, "/socket.io/?EIO=4");
+  socketIO.begin(socketIP, socketPORT, "/socket.io/?EIO=4");
 
   // event handler
   socketIO.onEvent(socketIOEvent);
@@ -183,9 +189,11 @@ unsigned long messageTimestamp = 0;
 void loop() {
   socketIO.loop();
 
+  pixels.clear();
+
   uint64_t now = millis();
 
-  if (now - messageTimestamp > 2000) {
+  if (now - messageTimestamp > 1000) {
     messageTimestamp = now;
 
     // creat JSON message for Socket.IO (event)
@@ -198,7 +206,7 @@ void loop() {
 
     // add payload (parameters) for the event
     JsonObject param1 = array.createNestedObject();
-    param1["masterVolume"] = analogRead(masterSliderPin);
+    param1["master"] = analogRead(masterSliderPin);
 
     JsonObject param2 = array.createNestedObject();
     param2["piano1"] = analogRead(piano1Pin);
@@ -224,5 +232,16 @@ void loop() {
 
     // Print JSON for debugging
     Serial.println(output);
+    Serial.println(rotaryCounter);
+
+    // Set NeoPixel to rotart encoder position
+    pixels.setPixelColor(rotaryCounter % 12, pixels.Color(0, 150, 0));
+    pixels.show();
+
   }
+  rotaryCounter++;
+  digitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                // wait for a half second
+  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);  
 }
