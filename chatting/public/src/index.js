414746ee485df0c5903501b8b1943bf34ec80d3c
PimTournaye <pim.tournaye@student.ehb.be>
2022-06-01 09:13:09 +0200

    transfered old code to different file

--- chatting/public/src/index.js
+++ chatting/public/src/index.js
@@ -1,11 +1,20 @@
+import { io } from 'socket.io-client'
 
-// DOM elements
+let socket = io();
+
+let form = document.getElementById('form');
 let input = document.getElementById('input');
-let messages = document.getElementById('messages');
 
-// Getting other users' messages through the socket.io server
-socket.on('chat message', function (msg) {
-  let item = document.createElement('li');
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  var item = document.createElement('li');
   item.textContent = msg;
   messages.appendChild(item);
   window.scrollTo(0, document.body.scrollHeight);
