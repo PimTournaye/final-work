e0b2e081d1829df782b90884345ae6593b5f0546
PimTournaye <pim.tournaye@student.ehb.be>
2022-06-08 13:05:24 +0200

    working working working

--- chatting/src/drums.js
+++ chatting/src/drums.js
@@ -12,7 +12,6 @@ import { Server } from "socket.io";
 // Web MIDI API for Node.js
 import { WebMidi } from 'webmidi'
 
-
 // Socket.io initialization
 const SOCKET_PORT = 3000;
 let io = new Server(SOCKET_PORT);
@@ -57,33 +56,35 @@ SerialPort.list().then(ports => {
   port.on('close', showPortClose);
   port.on('error', showError);
 
-
   function showPortOpen() {
     console.log('Serialport open. Data rate: ' + port.baudRate);
   }
 
+  // Read the data from the serial port and act accordingly
   function readSerialData(data) {
-    console.log('Data: ', data);
+    console.log('Raw serial data: ', data);
+    // remove any '\r' characters from the data, since the serialport library adds these
+    data = data.replace(/\r/g, '');
     switch (data) {
       // Send the message on crash cymbal hit
       case config.CRASH_MESSAGE:
         sendMessage();
-        counter = 0;
-        console.log(data);
+        tracker.counter = 0;
+        console.log('detected crash');
         break;
       // Remove the last character from the input on hihats cymbals closing
       case config.HIHAT_MESSAGE:
         message = message.slice(0, -1);
-        console.log(data);
+        console.log("detected hihat");
         break;
       // Insert a space into the message / input box when playing the kick drum
       case config.KICK_MESSAGE:
         message += ' ';
-        console.log(data);
+        console.log("detected kick");
         break;
-      default:
-        console.log('No match');
-        break;
+      // default:
+      //   console.log('No match with serial data');
+      //   break;
     }
   }
 
@@ -127,8 +128,12 @@ let sendMessage = () => {
 
 // Init empty string for building messages
 let message = ''
-// Counter for keeping track of represses of a key
-let counter = 0;
+
+// Timestamps and counter for key presses
+let tracker = {
+  time: null,
+  counter: null
+}
 
 function MIDIKeyPad() {
   // One big object with all the keys of the keypad 
@@ -142,59 +147,60 @@ function MIDIKeyPad() {
     previous: '',
     current: '',
   }
-  // Timestamps and counter for key presses
-  let tracker = {
-    time: null,
-    counter: null
-  }
 
   // MIDI event listeners
   MIDI.addListener('noteoff', (e) => {
     // Maybe do something here, probably not necessary
   });
 
-
   MIDI.addListener('noteon', (e) => {
     // Get the note as a string, eg. 'C3'
     note.current = e.note.identifier;
+    // Match note with a padValues.MIDI key in the padValues object to get that key's characters
+    characters = padValues.find(padValue => padValue.MIDI == note.current);
     // If the note is different from the previous note, reset the counter
     if (note.current !== note.previous) {
+      console.log("New note");
       // Reset the counter since there is a different 'key' being used
       tracker.counter = 0;
       // Restart the timer to track it
       tracker.time = Date.now();
     }
     if (note.current === note.previous) {
+      console.log("Same note as previous");
       const timecheck = Date.now();
       // if the difference between the current time and the previous time is less than the delay, increment the counter
       if (timecheck - tracker.time < config.KEYPAD_TIMER_DELAY) {
+        console.log('within timer delay');
         // Increment the counter
         tracker.counter++;
+        if (tracker.counter > characters.value.length) tracker.counter = 0;
         // Reset our timer
         tracker.time = Date.now();
-        // Remove the last character from the our message so we can replace it later
-        message = message.slice(0, -1)
+        // extra check to make it doesn't remove more characters than needed
+        if (tracker.counter !== 0) {
+          // Remove the last character from the our message so we can replace it
+          message = message.slice(0, -1);
+        }
       } else {
-        // Reset the counter to start anew
+        console.log('not within timer delay');
+        // Reset the counter and timer to start anew
         tracker.counter = 0;
         // Reset our timer
         tracker.time = Date.now();
       }
     }
-      // Match note with a padValues.MIDI key in the padValues object to get that key's characters
-      characters = padValues.find(padValue => padValue.MIDI == note.current);
-      console.log(characters);
-      // If the padValue is found, add the character to the message string
-      if (typeof characters != 'undefined' || characters != null) {
-        // Add the needed character padValue to the message string
-        let character = characters.value.charAt(tracker.counter);
-        console.log(character);
-        message += character;
-      } else console.log("Note not recognized or mapped", note.current);
+    // If the padValue is found, add the character to the message string
+    if (typeof characters != 'undefined' || characters != null) {
+      // Add the needed character padValue to the message string
+      let character = characters.value.charAt(tracker.counter);
+      console.log(character);
+      message += character;
+    } else console.log("Note not recognized or mapped", note.current);
 
-      // Set the previous note to the current note
-      note.previous = note.current
-      console.log('Message: ', message);
-      console.log("Counter:", counter);
+    // Set the previous note to the current note
+    note.previous = note.current
+    console.log('Message: ', message);
+    console.log("Counter:", tracker.counter);
   });
 }
