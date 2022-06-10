0fc130036f5b6e3d9269df87f74bf5bee49ab2cf
PimTournaye <pim.tournaye@student.ehb.be>
2022-06-10 13:02:33 +0200

    removed rotary encoder and neopixel

--- pong/ESP32 - Sockets/src/main.cpp
+++ pong/ESP32 - Sockets/src/main.cpp
@@ -5,8 +5,6 @@
 
 #include <ArduinoJson.h>
 
-#include <Adafruit_NeoPixel.h>
-
 #include <WebSocketsClient.h>
 #include <SocketIOclient.h>
 
@@ -14,49 +12,27 @@
 SocketIOclient socketIO;
 
 // Network configuration
-const char *ssid = "Medialab";
-const char *password = "Ml@bAdmin!";
-String socketIP = "10.3.208.48";
+const char *ssid = "Erasmushogeschool2";
+const char *password = "Marcom1070";
+String socketIP = "192.168.0.150";
+// const char *ssid = "Medialab";
+// const char *password = "Ml@bAdmin!";
+// String socketIP = "10.3.208.48";
 int socketPORT = 8080;
 
 // Control panel pins - Avoiding ADC2 pins since those are used for WiFi -- https://learn.adafruit.com/adafruit-huzzah32-esp32-feather/esp32-faq
-int masterSliderPin = 32;  // A7 or GPIO32
+int drumsPin = 32;  // A7 or GPIO32 --- noise
 int piano1Pin = 33;        // A9 or GPIO33
 int piano2Pin = 34;        // A2 or GPIO34
-int othersPin = 39;        // A3 or GPIO39
-int speedPin = 36;         // A4 or GPIO36
+int bassPin = 39;        // A3 or GPIO39
+int speedPin = 36;         // A4 or GPIO36 --- noise
 
-int pixelRing = 15;        // D15
 
-int rotaryPinA = 12;       // D12 - CLK
-int rotaryPinB = 14;       // D14 - DT
-int rotaryClick = 13;      // D13 - SW
-
-// Variables for the rotary encoder
-int rotaryCounter = 0;
-int rotaryAState;
-int rotaryALastState;
-
-// NeoPixel setup
-Adafruit_NeoPixel pixels(12, pixelRing, NEO_GRB + NEO_KHZ800);
 
 // map one potentiometer to a range of values
 int getSpeed() {
   float analogValue = analogRead(speedPin);
   return map(analogValue, 0, 1023, 0, 100);
-}
-
-// Function to read the rotary encoder
-void rotaryRead() {
-  rotaryAState = digitalRead(rotaryPinA);
-  if (rotaryAState != rotaryALastState) {
-    if (digitalRead(rotaryPinB) != rotaryALastState) {
-      rotaryCounter++;
-    } else {
-      rotaryCounter--;
-    }
-  }
-  rotaryALastState = rotaryAState;
 }
 
 // Copy pasted from WebSocket / Socket.io example, 
@@ -129,7 +105,7 @@ void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length) 
 // MAIN FUNCTIONS //
 ////////////////////
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   Serial.println("Booting...");
   Serial.println("");
@@ -154,20 +130,11 @@ void setup() {
   Serial.println(socketPORT);
 
   // Set up pins
-  pinMode(masterSliderPin, INPUT);
+  pinMode(drumsPin, INPUT);
   pinMode(speedPin, INPUT);
   pinMode(piano1Pin, INPUT);
   pinMode(piano2Pin, INPUT);
-  pinMode(othersPin, INPUT);
-
-  pinMode(pixelRing, OUTPUT);
-
-  pinMode(rotaryPinA, INPUT);
-  pinMode(rotaryPinB, INPUT);
-
-  rotaryALastState = digitalRead(rotaryPinA);
-
-  pixels.begin();
+  pinMode(bassPin, INPUT);
 
   // server address, port and URL
   //socketIO.begin(socketIP, socketPORT, "/socket.io/?EIO=4");
@@ -184,16 +151,11 @@ unsigned long messageTimestamp = 0;
 void loop() {
   socketIO.loop();
 
-  pixels.clear();
-
-  rotaryRead();
-
-  int master = analogRead(masterSliderPin);
-  int speed = getSpeed();
+  int drums = analogRead(drumsPin);
+  int speed =  analogRead(speedPin);
   int piano1 = analogRead(piano1Pin);
   int piano2 = analogRead(piano2Pin);
-  int others = analogRead(othersPin);
-  int rotary = rotaryCounter % 12;
+  int bass = analogRead(bassPin);
 
    uint64_t now = millis();
 
@@ -210,12 +172,11 @@ void loop() {
 
     // add payload (parameters) for the event
     JsonObject params = array.createNestedObject();
-    params["master"] = master;
+    params["drums"] = drums;
     params["piano1"] = piano1;
     params["piano2"] = piano2;
-    params["other"] = others;
+    params["bass"] = bass;
     params["speed"] = speed;
-    params["circle"] = rotary;
 
     // JSON to String (serializion)
     String output;
@@ -225,9 +186,7 @@ void loop() {
     socketIO.sendEVENT(output);
 
     // Print JSON for debugging
-    Serial.println(output);
+    //Serial.println(output);
   }
-    // Set NeoPixel to rotart encoder position
-    pixels.setPixelColor(rotaryCounter % 12, pixels.Color(0, 150, 0));
-    pixels.show();
+
 }
