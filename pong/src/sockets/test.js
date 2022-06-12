59fdf6a364d9baffd717a8f078c37d895b456193
PimTournaye <pim.tournaye@student.ehb.be>
2022-06-13 01:26:58 +0200

    fixed launch

--- pong/src/sockets/test.js
+++ pong/src/sockets/test.js
@@ -1,12 +1,12 @@
-import { createServer } from "http";
-import { Server } from "socket.io";
+const { createServer } = require("http");
+const { Server } = require("socket.io");
 
 const httpServer = createServer();
 const io = new Server(httpServer, {
     cors: {
         origin: "http://localhost:3000",
         methods: ["GET", "POST"]
-      }
+    }
 });
 
 io.on("connection", (socket) => {
@@ -15,7 +15,7 @@ io.on("connection", (socket) => {
     socket.on('arduino_event', (data) => {
         console.log("Message from Client : ", data);
         socket.broadcast.emit("Send Message socket.broadcast.emit : ", data);
-     io.emit("Send Message io.emit Broadcasted : ", data);
+        io.emit("Send Message io.emit Broadcasted : ", data);
     });
 
     socket.on('disconnect', () => {
